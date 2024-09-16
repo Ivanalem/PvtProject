6ef0ae7b1fe2c0cd7f6c9f0387ad5046aa662649
Ivan <wegespro@mail.ru>
2024-09-16 23:03:44 +0300

    Work on bugs at PractiseLesson2

--- src/PracticeLesson2/TaskTwo.java
+++ src/PracticeLesson2/TaskTwo.java
@@ -1,26 +1,26 @@
 package PracticeLesson2;
 
-import java.sql.SQLOutput;
+import java.util.Arrays;
 
 public class TaskTwo {
     public static void main(String[] args) {
         //Найти сумму и произведение двух чисел
-        byte a = 15, b = 20;
-        byte sum = (byte) (a+b);
-        byte compos = (byte) (a*b);
+        int a = 15, b = 20;
+        int sum =  a+b;
+        int compos = a*b;
         System.out.println("Сумма чисел: "+sum + " Произведение двух чисел: " + compos);
 
         //Вычислить 20+(-3)*5/8
 
-        byte comp = 20 + (-3)* 5/8;
+        double comp = 20 + (-3)* 5/8f;
         System.out.println("Вычислено: "+comp);
 
         //вывести последнюю цифру числа 31
-
-        byte num = 31%10;
+        int num = 31%10;
         System.out.println("Последняя цифра числа: "+num); //(остаток от деления)
-        String number = "31"; //Возможно ли так?
-        System.out.println("Последняя цифра числа: "+ number.charAt(1));
+        int numberint = 31;
+        String numStr = Integer.toString(numberint);
+        System.out.println("Последняя цифра числа: "+ numStr.charAt(1));//вывод через строку
 
 
         //Вывести 3-юю цифру 5-значного числа 54324
@@ -29,31 +29,25 @@ public class TaskTwo {
 
         // Найти сумму цифр ab+cd числа abcd
 
-        int abcd = 1345;
-        int sumNumber = 0;
-        for(int j=0; abcd !=0 ; j++){
-            sumNumber += abcd%10;
-            abcd /=10;
-        }
-        System.out.println("Сумма чисел ab+cd = "+sumNumber);
+        String abcd = "1345";
+        int strAbcd = Integer.parseInt(abcd);
+        int strAb = strAbcd/100;
+        int strCd = strAbcd%100;
+        int sumAbCd = strAb+strCd;
+        System.out.println("Сумма чисел ab + cd: "+sumAbCd);
+
 
 
         //Найти площадь и периметр круга, r=7.5
 
-        int s = (int)Math.round(Math.PI * Math.pow(7.5,2));
-        int perim = (int)Math.round(Math.PI * 7.5);
-
+        double s = Math.round(Math.PI * Math.pow(7.5,2));
+        double perim = Math.round(Math.PI * 7.5);
         System.out.println("Площадь круга: " + s + " Периметр круга: " + perim);
 
         //Найти среднее арифметическое пяти чисел
-        int coun = 0;
-        int deliver = 0;
         int [] mass  ={34,51,64,45,76};
-        for(int i = 0; i<mass.length;i++){
-            coun += mass[i];
-            deliver = coun/mass.length;
-
-        }
+        int sumMass = Arrays.stream(mass).sum();
+        int deliver = sumMass/mass.length;
         System.out.println("Сумма чисел в массиве: "+deliver);
 
 
