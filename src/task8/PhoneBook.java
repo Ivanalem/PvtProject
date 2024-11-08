a7ecd23d221282db01b82ff8383661790c83c5c4
Ivan <wegespro@mail.ru>
2024-11-08 19:39:24 +0300

    Part2

--- src/task8/PhoneBook.java
+++ src/task8/PhoneBook.java
@@ -1,2 +1,32 @@
-package task8;public class PhoneBook {
+package task8;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class PhoneBook {
+    private Map<String, List<Integer>> keyMapTask2;
+
+    protected void addNumber(int number, String name, Map<String, List<Integer>> keyMapTask2) {
+        if (keyMapTask2.containsKey(name)) {
+            keyMapTask2.get(name).add(number);
+        } else {
+            List<Integer> list = new ArrayList<>();
+            list.add(number);
+            keyMapTask2.put(name, list);
+        }
+    }
+
+    protected PhoneBook(Map<String, List<Integer>> keyMapTask2) {
+        this.keyMapTask2 = keyMapTask2;
+    }
+
+    @Override
+    public String toString() {
+        return "Телефонная книга:  " + getKeyMapTask2();
+    }
+
+    public Map<String, List<Integer>> getKeyMapTask2() {
+        return keyMapTask2;
+    }
 }
--- src/task8/Task8.java
+++ src/task8/Task8.java
@@ -14,8 +14,17 @@ public class Task8 {
         keyMap.put(1234567, "Sergey");
         keyMap.put(12345678, "Sergey");
         task1(keyMap);
-        HashMap<String, ArrayList<Integer>> keyMapTask2 = new HashMap<>();
-        task2(keyMapTask2);
+        HashMap<String, List<Integer>> keyMapTask2 = new HashMap<>();
+        PhoneBook phoneBook = new PhoneBook(keyMapTask2);
+        phoneBook.addNumber(121, "Valentin", keyMapTask2);
+        phoneBook.addNumber(12, "Valentin", keyMapTask2);
+        phoneBook.addNumber(1234, "Valentin", keyMapTask2);
+        phoneBook.addNumber(12345, "Zora", keyMapTask2);
+        phoneBook.addNumber(12345, "Zora", keyMapTask2);
+        phoneBook.addNumber(1234567, "Sergey", keyMapTask2);
+        phoneBook.addNumber(123, "Danil", keyMapTask2);
+        phoneBook.addNumber(1239, "Danil", keyMapTask2);
+        System.out.println(phoneBook);
     }
 
     public static void task1(HashMap<Integer, String> keyMap) {
@@ -27,29 +36,5 @@ public class Task8 {
         System.out.println(keyMap);
         System.out.println(mapTask1);
         System.out.println("Конец работы task1!");
-    }
-
-    public static void task2(Map<String, ArrayList<Integer>> keyMapTask2) {
-        System.out.println("Начало работы task2: ");
-        addNumber(12, "Valentin",keyMapTask2);
-        addNumber(1234, "Valentin",keyMapTask2);
-        addNumber(12345, "Zora",keyMapTask2);
-        addNumber(12345, "Zora",keyMapTask2);
-        addNumber(1234567, "Sergey",keyMapTask2);
-        addNumber(123, "Danil",keyMapTask2);
-        addNumber(1239, "Danil",keyMapTask2);
-        System.out.print(keyMapTask2);
-        System.out.println();
-        System.out.println("Конец работы task2!");
-
-    }
-    public static void addNumber(int value,String key, Map<String, ArrayList<Integer>> keyMapTask2){
-        if (keyMapTask2.containsKey(key)) {
-            keyMapTask2.get(key).add(value);
-        } else {
-            ArrayList<Integer> list = new ArrayList<>();
-            list.add(value);
-            keyMapTask2.put(key, list);
-        }
     }
 }
