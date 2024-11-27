3cf2af25a31eafa5693911c48e81d874d109495a
Ivanalem <wegespro@mail.ru>
2024-11-28 00:26:34 +0300

    first

--- src/task10/Task2.java
+++ src/task10/Task2.java
@@ -1,15 +1,22 @@
 package task10;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Task2 {
     public static void main(String[] args) {
         List<String> myList = Arrays.asList("a1", "a2", "a3", "b1", "b3", "c2", "c1", "c5");
+        List<Object> myNewList = myList.stream()
+                .filter(m -> !m.contains("3"))
+                .sorted(Comparator.comparing(m -> m.charAt(1)))
+                .sorted(Comparator.comparing(m -> m.charAt(0)))
+                .map(String::toUpperCase)
+                .peek(System.out::print)
+                .collect(Collectors.toList());
         System.out.println(myList);
-        List<String> myNewList;
 
+
+        System.out.println(Arrays.toString(myNewList.toArray()) + " " + myNewList.toArray().length);
     }
 }
